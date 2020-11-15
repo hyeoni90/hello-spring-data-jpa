a3db3c1082676dcaba5901f71707ef39659bc193
hyeoni90 <gusk743@gmail.com>
2020-11-15 14:14:58 +0900

    feat: Add member domain

---
+++ src/main/java/com/hyeonah/hellojpa/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package com.hyeonah.hellojpa.repository;
+
+import com.hyeonah.hellojpa.domain.Member;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by hyeonahlee on 2020-11-15.
+ */
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+            .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+            .setParameter("name", name)
+            .getResultList();
+    }
+}
---
+++ src/main/java/com/hyeonah/hellojpa/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.hyeonah.hellojpa.service;
+
+import com.hyeonah.hellojpa.domain.Member;
+import com.hyeonah.hellojpa.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by hyeonahlee on 2020-11-15.
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
