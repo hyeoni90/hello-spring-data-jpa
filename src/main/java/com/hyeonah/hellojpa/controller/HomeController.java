d5e34a75fab531f8862be2c5a8027732874f3664
hyeoni90 <hyeona743@gmail.com>
2021-02-13 16:18:17 +0900

    feat: Add home page

---
+++ src/main/java/com/hyeonah/hellojpa/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.hyeonah.hellojpa.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hyeoni90 on 2021-02-13
+ */
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
